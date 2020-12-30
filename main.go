47d4443d1c0749e63bed9f26f5dde9312f59a1bb
coding-muse <dent4real@yahoo.com>
2020-12-30 13:17:23 +0100

    Bug: Fix Go syntax

--- main.go
+++ main.go
@@ -5,5 +5,10 @@ import (
 type Server struct{}
 func (s *Server) ServeHTTP(
 	w http.ResponseWriter, 
-	r *http.Request ) {   w.WriteHeader(http.StatusOK)   w.Header().Set("Content-Type", "application/json")   w.Write([]byte(`{"message": "hello world"}`))}
-func main() {   s := &Server{}   http.Handle("/", s)   log.Fatal(http.ListenAndServe(":8080", nil))}
+	r *http.Request ) {   
+		w.WriteHeader(http.StatusOK)   w.Header().Set("Content-Type", "application/json")   w.Write([]byte(`{"message": "hello world"}`))}
+func main() {   
+	s := &Server{}   
+	http.Handle("/", s)   
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
